3d6898da640611eb4c340adefed48683365a3914
Bob Witmer <bwitmer@golden.net>
2025-08-13 10:05:01 -0400

    Initial Commit

---
+++ Wu-TangAVP/Wu_TangAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangAVPApp.swift
+//  Wu-TangAVP
+//
+//  Created by Bob Witmer on 2025-08-13.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
